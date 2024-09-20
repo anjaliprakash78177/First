1ff9d33c020e0a40b3e6005125f9e3db45cca601
anjaliprakash78177 <anjaliprakash78177@gmail.com>
2024-09-20 14:58:31 +0530

    Add files via upload

---
+++ for loop (2).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+//for loop
+void main()
+{
+int i;
+clrscr();
+    for(i=0;i<=10;i++)
+        {
+        printf("Gate Academy\n");
+        }
+getch();
+}
+    
